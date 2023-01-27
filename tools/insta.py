52f60cc629428d8d184a5da85f5ce182495260d9
unti1 <kostyuchenko.work@gmail.com>
2023-01-27 17:12:55 +0300

    modified:   tools/insta.py

--- tools/insta.py
+++ tools/insta.py
@@ -52,8 +52,8 @@ class InstPars(Thread):
 
         if invisable:
             options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         
         if USERPASSPROXY:
             options.add_argument('--proxy-server=%s' % USERPASSPROXY)
